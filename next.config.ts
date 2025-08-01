3d603406e93a8ff1635ea7e697cf404313ce630b
Robin S Sao <saosovanraksa@gmail.com>
2025-08-01 19:21:03 +0700

    fixed images not loading in production
    the bug was in the frontend, not the backend. cross-origin images are only loaded if they're specified in next.config.ts. my hosting provider directly pulls from the public git repo, but the repo itself only contains next.config.ts.example, not next.config.ts.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL("https://YOUR_STRAPI_DOMAIN/**")],
+    remotePatterns: [new URL(`${process.env.STRAPI_BASE_URL}/**`)],
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
   },
 };
 
